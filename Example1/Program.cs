8c9f673685a676d02183f31092eac235ae5ee9d3
Daniel <daniel.bozhko1903@mail.ru>
2022-12-08 16:34:20 +0700

    Result

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+
+if ((num < 100000) && (num >= 10000))
+{
+    int a = num % 10;
+    int b = num / 10000;
+    int c = (num % 100) / 10;
+    int d = (num / 1000) % 10;
+
+    if (a == b && c == d)
+    {
+        Console.WriteLine($"Число палиндром {num}");
+    }
+    else
+    {
+        Console.WriteLine($"Число не палиндром {num}");
+    }
+
+    return;
+}
+
+Console.WriteLine("Введено не пятизначное число");
--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
